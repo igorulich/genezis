9c735d3e3947415d02bbefe897ccc9ceffdbbfd9
Ulich Igor <ylichigor@gmail.com>
2024-06-20 18:29:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from './scss/index.module.scss'
+
+function App() {
+  return (
+    <div className={styled.App}>
+      <h1>Genezis</h1>
+      <p>
+        Edit <code>src/App.jsx</code> and save to <b>reload.</b>
+      </p>
+      
+    </div>
+  );
+}
+
+export default App;
